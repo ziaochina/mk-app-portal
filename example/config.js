36d55bf1e90dd8998d96f9b1d68e4d62c8b8fa08
ziaochina <ziaochina@gmail.com>
2017-07-22 13:15:42 +0800

    zlj

--- example/config.js
+++ example/config.js
@@ -3,9 +3,9 @@ import { fetch } from 'mk-utils'
 
 import './mock.js' //启用mock
 fetch.config({
-	mock:true, //启用mock
-	after:(response)=>{
-		if(response.result){
+	mock: true, //启用mock
+	after: (response) => {
+		if (response.result) {
 			return response.value
 		}
 	}
@@ -16,15 +16,9 @@ var _options = {}
 function config(options) {
 	Object.assign(_options, options)
 
-	if (_options.apps) {
-
-		Object.keys(_options.apps).forEach(key => {
-			_options.apps[key].config({
-				apps: _options.apps,
-				//webapi //正式网站应该有一个完整webapi对象，提供所有web请求函数
-			})
-		})
-	}
+	_options.apps && _options.apps.config({
+		//'*': { webapi } //正式网站应该有一个完整webapi对象，提供所有web请求函数
+	})
 
 	_options.targetDomId = 'app'
 	_options.startAppName = 'mk-app-portal' //mk-app-root
