91aa47705d5e36afd604ee2c7e13cf2b04b9c7f4
ziaochina <ziaochina@gmail.com>
2017-08-04 15:33:06 +0800

    zlj

--- example/index.js
+++ example/index.js
@@ -3,23 +3,16 @@ import * as mkComponents from 'mk-component'
 import myConfig  from './config'
 
 import mk_app_portal_about from './apps/mk-app-portal/apps/mk-app-portal-about/index.js'
-
 import mk_app_portal_app1 from './apps/mk-app-portal/apps/mk-app-portal-app1/index.js'
-
 import mk_app_portal_app2 from './apps/mk-app-portal/apps/mk-app-portal-app2/index.js'
-
 import mk_app_portal from './apps/mk-app-portal/index.js'
 
 const apps = {
-	
-	[mk_app_portal_about.name]: mk_app_portal_about,
-	
-	[mk_app_portal_app1.name]: mk_app_portal_app1,
-	
-	[mk_app_portal_app2.name]: mk_app_portal_app2,
-	
+		
+	[mk_app_portal_about.name]: mk_app_portal_about,	
+	[mk_app_portal_app1.name]: mk_app_portal_app1,	
+	[mk_app_portal_app2.name]: mk_app_portal_app2,	
 	[mk_app_portal.name]: mk_app_portal,
-
 }
 
 apps.config = (options) => {
