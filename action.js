91c14c6d76867330a0cb5118101587b1a89496f5
ziaochina <ziaochina@gmail.com>
2017-08-26 20:43:53 +0800

    zlj

--- action.js
+++ action.js
@@ -33,7 +33,7 @@ class action {
             this.metaAction.context.set('currentUser', response.user)
             this.metaAction.sf('data.other.currentUser', fromJS(response.user))
         }
-        else{
+        else {
             this.metaAction.context.set('currentUser', undefined)
             if (this.component.props.onRedirect && this.config.goAfterLogout) {
                 this.component.props.onRedirect(this.config.goAfterLogout)
@@ -114,6 +114,10 @@ class action {
         if (hit) {
             this.injections.reduce('setContent', hit.appName, hit.appParams)
         }
+    }
+
+    setContent = (appName, appParams) => {
+        this.injections.reduce('setContent', appName, appParams)
     }
 }
 
