bd5afb2a8e42c4adb0fdde0470cfcc9318fca261
ziaochina <ziaochina@gmail.com>
2017-09-27 23:05:16 +0800

    zlj

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import * as data from './data'
 
 export default {
 	name: "mk-app-portal",
-	version: "1.0.24",
+	version: "1.0.25",
 	description: "mk-app-portal",
 	meta: data.getMeta(),
 	components: [],
