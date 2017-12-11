a4e647f0526b011729c31c1020b5f409e5d97b61
ziaochina <ziaochina@gmail.com>
2017-12-11 08:43:49 +0800

    zlj

---
+++ index.umd.js
@@ -0,0 +1,17 @@
+
+import config from './config'
+import * as data from './data'
+import './mock.js'
+import './style.less'
+
+export default {
+    name: "mk-app-portal",
+    version: "1.0.46",
+    description: "mk-app-portal",
+    meta: data.getMeta(),
+    components: [],
+    config: config,
+    load: (cb) => {
+        cb(require('./component'), require('./action'), require('./reducer'))
+	}
+}
